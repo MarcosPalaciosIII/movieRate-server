9c499d6a4a9e69a613d66526db6ac81907402852
Marcos <mp2772@gmail.com>
2019-02-18 17:51:34 -0500

    modified user model

--- models/User.js
+++ models/User.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  username: {type: String},
+  password: {type: String},
+  loggedIn: {type: Boolean}
 }, {
   timestamps: {
     createdAt: 'created_at',
