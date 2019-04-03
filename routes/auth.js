31415bc1d54a5672556a8c375c13381320977995
Marcos <mp2772@gmail.com>
2019-04-03 09:24:03 -0400

    modified auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require('passport');
 const router = express.Router();
-const User = require("../models/User");
+const UserModel = require("../models/User");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcryptjs");
