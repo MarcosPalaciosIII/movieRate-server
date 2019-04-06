9d684c2137c1ac2cc3fea0dc9eb3ba7c51e05a36
Marcos <mp2772@gmail.com>
2019-04-06 18:40:08 -0400

    modified script to allow carousel to work

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+$(document).ready(() => {
+  $(".carousel-item:nth-child(1)").addClass("active");
+});
