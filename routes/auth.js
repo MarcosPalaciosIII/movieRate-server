d9043bc3801af457022324bb3de5863189234ddc
Marcos <mp2772@gmail.com>
2019-05-11 18:55:19 -0400

    made changes to auth route

--- routes/auth.js
+++ routes/auth.js
@@ -13,15 +13,102 @@ router.get("/login", (req, res, next) => {
 });
 
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+// router.post("/login", passport.authenticate("local", {
+//   successRedirect: "/",
+//   failureRedirect: "/auth/login",
+//   failureFlash: true,
+//   passReqToCallback: true
+// }));
+
+router.post("/login", (req,res, next) => {
+  // console.log("logging in user ===============>", req.body.LoginEmail, req.body.LoginPassword);
+  // console.log("the is the info from the form ----------- ", req.body);
+
+  // redirect to home if you are already logged in
+  if(req.user){
+    res.redirect("/");
+    // early return to stop the function since there's an error
+    //(prevents the rest of the code form running)
+    return;
+  }
+  // console.log("not logged in");
+
+  // find a user document in the database with that email
+  UserModel.findOne({ email: req.body.LoginEmail })
+  .then((userFromDb) => {
+    if (userFromDb === null) {
+      // if we didn't find a user
+      res.locals.errorMessage = "Email incorrect.";
+      res.render("auth/login.hbs");
+
+      // early return to stop the function since there's an error
+      // (prevents the rest of the code from running)
+      return;
+    }
+    // console.log("user found on db >>>>>>>>>> ", userFromDb);
+
+    // if email is correct now we check the password
+    const isPasswordGood =
+     bcrypt.compareSync(req.body.LoginPassword, userFromDb.password);
+
+     if (isPasswordGood === false) {
+       res.locals.errorMessage = "Password incorrect.";
+       res.render("auth/login.hbs");
+
+       // early return to stop the function since there's an error
+       // (prevents the rest of the code from running)
+       return;
+     }
+     // console.log("no issues with pw");
+
+     // CREDENTIALS ARE GOOD! We need to log the users in
+
+
+      // Passport defines the "req.login()"
+      // for us to specify when to log in a user into the session
+      req.login(userFromDb, (err) => {
+        if (err) {
+          // console.log("something went wrong with login() from passport", err);
+
+          // if it didn't work show the error page
+          next(err);
+        }
+        else {
+
+              console.log("user" + userFromDb);
+              userFromDb.set({loggedIn: true});
+              userFromDb.save()
+
+              .then(() => {
+                // console.log("User has been logged in }}}}}}}}}}}}} ", req.user, "=============", userFromDb);
+
+                req.session.cookie.expires = false;  //this should make cookies last for duration of user.
+
+                //redirect to the home page on successful log in
+                res.redirect("/");
+              })
+              .catch((err) => {
+                next(err);
+              });
+
+        }
+      }); //req.login()
+  })// then()
+  .catch((err) => {
+    next(err);
+  });
+});
 
 
 router.get("/signup", (req, res, next) => {
+  // redirect to home if you are already logged in
+if(req.user){
+    res.redirect("/");
+    // early return to stop the function since there's an error
+    //(prevents the rest of the code form running
+    return;
+  }
+
   res.render("auth/signup");
 });
 
@@ -35,13 +122,13 @@ router.post("/signup", (req, res, next) => {
     return;
   }
 
-  User.findOne({ email }, "email", (err, user) => {
+  UserModel.findOne({ email }, "email", (err, user) => {
 
     if(user !== null) {
       res.render("auth/signup", { message: "The email already exists" });
       return;
     } else {
-      User.findOne({ username }, "username", (err, user) => {
+      UserModel.findOne({ username }, "username", (err, user) => {
         if (user !== null) {
           res.render("auth/signup", { message: "The username already exists" });
           return;
@@ -49,11 +136,21 @@ router.post("/signup", (req, res, next) => {
 
         const salt = bcrypt.genSaltSync(bcryptSalt);
         const hashPass = bcrypt.hashSync(password, salt);
+        const hashName = bcrypt.hashSync(username, salt);
 
-        const newUser = new User({
+        const newUser = new UserModel({
           username,
           email,
-          password: hashPass
+          password: hashPass,
+          favMovies: [],
+          favActors: [],
+          playlists: [],
+          loggedIn: false,
+          confirmCode: hashName,
+          negativeMarks: 0,
+          strikes: 0,
+          reportedBy: [],
+          reported: []
         });
 
         newUser.save()
@@ -69,9 +166,27 @@ router.post("/signup", (req, res, next) => {
 });
 
 
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+
+router.get("/logout", (req, res, next) => {
+  // console.log("this is the user before the log out ============= ", req.user);
+  if(req.user === undefined) {
+    res.redirect("/");
+    return;
+  }
+
+  req.user.set({loggedIn: false});
+  req.user.save()
+
+  .then(() => {
+    //Passport defines the "req.logout()" method
+    // for us to specify when to log out a user (clear them from the session)
+    req.logout();
+
+    res.redirect("/");
+  })
+  .catch((err) => {
+    next(err);
+  });
 });
 
 
