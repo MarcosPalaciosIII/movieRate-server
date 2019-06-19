8544eaa566992d7598fa678643938038deea3f57
Marcos <mp2772@gmail.com>
2019-06-19 09:25:19 -0400

    added code to assist switch button in making playlist public or private

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,12 @@
+// const axios = require('axios');
+// import axios from 'https://unpkg.com/axios/dist/axios.min.js';
+// import {} from 'dotenv/config';
+// import dotenv from 'dotenv/config';
+// dotenv.config();
+// import theEnv from '.env';
+
+// console.log("these are the environment variables ======== ", theEnv);
+
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
@@ -10,7 +19,7 @@ function changeAction(val) {
 }
 
 var faveButton = $('.addToFaveButton')[0];
-console.log("><>><<><><><><><<<><>><>", faveButton);
+// console.log("><>><<><><><><><<<><>><>", faveButton);
 
 function spinner(event) {
   console.log("the parent node ========= ", $(event)[0].parentNode.className.includes('show'));
@@ -26,10 +35,41 @@ function spinner(event) {
   }
 }
 
+var toggleInputValue = $("input.custom-control-input").prop('value');
+console.log("===================== ", toggleInputValue);
+console.log("is it public >>>>>>>>>>>>> ", $("input.custom-control-input").prop('checked'), "======", $("input.custom-control-input").prop('name'));
+
+
+// console.log("the domain name >>>>>>>>>>>> ", `${new URL($('#theDomainName')[0].innerHTML)}playlists/changePublicStatus`);
+
+
+$("input.custom-control-input").change(function(){
+  theData = {
+    isPublic: $("input.custom-control-input").prop('checked'),
+    playlistId: $("input.custom-control-input").prop('value')
+  };
+    if($(this).prop("checked") == true){
+       console.log("the toggle button is true", theData);
+       axios.post(`${new URL($('#theDomainName')[0].innerHTML)}playlists/changePublicStatus`, theData);
+       // axios.
+    }else{
+       console.log('the toggle button is false', theData);
+       axios.post(`${new URL($('#theDomainName')[0].innerHTML)}playlists/changePublicStatus`, theData);
+    }
+});
+
 // $('#theAddToFaveButton').click((event) => {
 //   console.log("the event --- ", event);
 // });
 
 $(document).ready(() => {
   $(".carousel-item:nth-child(1)").addClass("active");
+
+  if($("input.custom-control-input").prop('name') == 'true') {
+    // console.log("the name is set to true <<<<<<<<< ", $("input.custom-control-input").prop('name'));
+    $("input.custom-control-input").prop('checked', true);
+  } else {
+    // console.log("the name is set to false >>>>>>>>>>  <<<<<<<<< ", $("input.custom-control-input").prop('name'));
+    $("input.custom-control-input").prop('checked', false);
+  }
 });
